76b3ca3fe3d8487854b5c3e6024b6978608fe09f
Quinten <90333974+qdamsma@users.noreply.github.com>
2025-04-23 19:48:59 +0200

    hello world

---
+++ src/views/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/index.css';
+
+function HomePage() {
+
+    return (
+        <main>
+            <section>
+                <h1>Quinten Damsma Portfolio</h1>
+            </section>
+        </main>
+    );
+}
+
+export default HomePage;
